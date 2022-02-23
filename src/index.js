2903d1d263d4825530d9a59c2ee5857d6c6180aa
MatveyRymar <93730447+MatveyRymar@users.noreply.github.com>
2022-02-24 01:21:01 +0300

    fix: load index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+    const textNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const decNumbers = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number < 20) {
+        return textNumbers[number];
+    } else if (number < 100) {
+        return `${decNumbers[Math.floor(number / 10) - 2]} ${toReadable(number % 10)}`.replace(' zero', '');
+    } else {
+        return `${textNumbers[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`.replace(' zero', '');
+    }
 }
